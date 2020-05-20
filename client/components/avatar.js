215a299e97bfa96ee067448e05afd5fef314596a
Matthew Blode <m@blode.co>
2020-05-20 23:11:57 +1000

    Avatar

--- client/components/avatar.js
+++ client/components/avatar.js
@@ -1,12 +1,8 @@
-import Link from 'next/link';
-
 export default function Avatar({ author }) {
     return (
         <div className='flex items-center'>
             <img src={author.photo.url} className='w-12 h-12 rounded-full mr-4' alt={author.fullName} />
-            <Link as={`/author/${author.username}`} href='/author/[username]'>
-                <div className='text-xl font-bold'>{author.fullName}</div>
-            </Link>
+            <div className='text-xl font-bold'>{author.fullName}</div>
         </div>
     );
 }
